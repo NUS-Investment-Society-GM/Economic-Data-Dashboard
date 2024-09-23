00e27bcbc55de9ab0658ae8e9e74ff847e06ab0f
Max Yang <>
2024-09-23 15:02:45 +0800

    match data name and code

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,7 +1,10 @@
 import pandas_datareader.data as web
 from datetime import datetime
 import pandas as pd
-code_dict={'Continued Claims':'CCSA'}
+code_dict={'Continued Claims':['CCSA'], 'Total Nonfarm Payroll': ['PAYEMS'], 'Initial Jobless Claims':['ICSA'],
+           'JOLTS': ['JTSJOL'], 'wages': ['LES1252881600Q'], 'Government vs Total private': ['USGOVT', 'USPRIV'],
+           'Labour supply': ['CIVPART'], 'Labor demand': ['CE16OV']}
+
 def fetch_from_FRED(name,start_date, end_date):
     if not start_date:
         start_date = '2015-01-01'
@@ -12,8 +15,9 @@ def fetch_from_FRED(name,start_date, end_date):
         return pd.DataFrame()
     else:
         code=code_dict[name]
-    df = web.DataReader(code, 'fred', start_date, end_date)
-    df.columns = [name]
+    df = web.DataReader([i for i in code], 'fred', start_date, end_date)
+    df.columns = [i for i in code]
+
     return df
 
 start_date = '2015-01-01'
